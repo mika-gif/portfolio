4f604265b97d32a3ba8d5a40d41785dd1f472032
mika-gif <minorikaori03@gmail.com>
2021-10-08 01:48:20 +0900

    変更　ふわ表示keyframeなし

--- js/script.js
+++ js/script.js
@@ -69,54 +69,6 @@ $(window).on('load', function () {
 });
 
 
-
-
-
-
-// ==========上から下ローディング画面処理========= //
-
-// $(window).on('load', function () {
-//   $("#splash-logo").delay(500).fadeOut('slow');//ロゴを1.2秒でフェードアウトする記述
-
-//   //=====ここからローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
-//   $("#splash").delay(500).fadeOut('slow', function () {//ローディングエリア（splashエリア）を1.5秒でフェードアウトする記述
-
-//     $('body').addClass('appear');//フェードアウト後bodyにappearクラス付与
-
-//   });
-//   //=====ここまでローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
-
-//   //=====ここから背景が伸びた後に動かしたいJS
-//   $('.splashbg').on('animationend', function () {
-//     //この中に動かしたいJSを記載
-
-//     // ==========タイピング処理========= //
-//     var element = $(".TextTyping");
-//     element.each(function () {
-//       var text = $(this).html();
-//       var textbox = "";
-//       text.split('').forEach(function (t) {
-//         if (t !== " ") {
-//           textbox += '<span>' + t + '</span>';
-//         } else {
-//           textbox += t;
-//         }
-//       });
-//       $(this).html(textbox);
-
-//     });
-//     TextTypingAnime();/* アニメーション用の関数を呼ぶ*/
-
-//   });
-//   //=====ここまで背景が伸びた後に動かしたいJS
-
-// });
-
-
-
-
-
-
 // ここから==========スクロール時、h2文字回転処理========= //
 
 
@@ -191,18 +143,65 @@ $(window).on('load', function () {
 
 // ここから==========ふわっと表示する処理========= //
 
-// 動きのきっかけとなるアニメーションの名前を定義
+// // 動きのきっかけとなるアニメーションの名前を定義
+// function fadeAnime() {
+
+//   // ふわっ
+//   $('.fadeUpTrigger').each(function () { //fadeUpTriggerというクラス名が
+//     var elemPos = $(this).offset().top;//要素より、50px上の
+//     var scroll = $(window).scrollTop();
+//     var windowHeight = $(window).height();
+//     if (scroll >= elemPos - windowHeight) {
+//       $(this).addClass('fadeUp');// 画面内に入ったらfadeUpというクラス名を追記
+//     } else {
+//       $(this).removeClass('fadeUp');// 画面外に出たらfadeUpというクラス名を外す
+//     }
+//   });
+
+// }
+
+
+
+// // 画面をスクロールをしたら動かしたい場合の記述
+// $(window).scroll(function () {
+//   fadeAnime();/* アニメーション用の関数を呼ぶ*/
+// });
+
+// ここまで==========ふわっと表示する処理========= //
+
+
+// $(function () {
+//   // ↓windowがスクロールされた時に実行する処理
+//   $(window).scroll(function () {
+//     // ↓複数のオブジェクトに対して繰り返し処理を行う
+//     $(".scroll-block").each(function () {
+//       // ↓要素が画面上に表示されたかどうかを判定させる変数を定義----------
+//       var scroll = $(window).scrollTop(); // 画面トップからのスクロール量
+//       var blockPosition = $(this).offset().top; // 画面トップから見たブロックのある位置
+//       var windowHeihgt = $(window).height(); // ウィンドウの高さ
+//       // ↓if式で計算する流れ----------
+//       // 手順① 表示させたいブロックの位置している高さ - ウィンドウの高さ
+//       // 手順② スクロール量が①より多くなった時＝要素が画面上に表示されたと認識
+//       // 手順③ 要素に「blockIn」クラスを付与。動きはCSSで制御。
+//       if (scroll > blockPosition - windowHeihgt + 300) {
+//         $(this).addClass("blockIn");
+//       }
+//     });
+//   });
+// });
+
+
 function fadeAnime() {
 
   // ふわっ
-  $('.fadeUpTrigger').each(function () { //fadeUpTriggerというクラス名が
+  $('.fade-block2').each(function () { //fadeUpTriggerというクラス名が
     var elemPos = $(this).offset().top;//要素より、50px上の
     var scroll = $(window).scrollTop();
     var windowHeight = $(window).height();
     if (scroll >= elemPos - windowHeight) {
-      $(this).addClass('fadeUp');// 画面内に入ったらfadeUpというクラス名を追記
+      $(this).addClass('blockIn');// 画面内に入ったらfadeUpというクラス名を追記
     } else {
-      $(this).removeClass('fadeUp');// 画面外に出たらfadeUpというクラス名を外す
+      $(this).removeClass('blockIn');// 画面外に出たらfadeUpというクラス名を外す
     }
   });
 
@@ -214,5 +213,3 @@ function fadeAnime() {
 $(window).scroll(function () {
   fadeAnime();/* アニメーション用の関数を呼ぶ*/
 });
-
-// ここまで==========ふわっと表示する処理========= //
