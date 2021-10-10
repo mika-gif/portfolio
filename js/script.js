5443cc2f187e30095f3b19c0354bb55c5889a9f2
mika-gif <minorikaori03@gmail.com>
2021-10-11 01:01:31 +0900

    修正　ローディング画面がセッション間に一度のみ表示されるよう対応(02)

--- js/script.js
+++ js/script.js
@@ -79,6 +79,8 @@ if (!sessionStorage.getItem(keyName)) {
   //ここに通常アクセス時の処理
   // console.log("訪問済みです");
   $("#splash").css("display", "none");//同日2回目のアクセスでローディング画面非表示
+  $('body').css("background", "#ffffff");
+  $('.wrapper').css("opacity", "1");
 }
 
 
